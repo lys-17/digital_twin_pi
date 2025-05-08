4ba335f7e28c5d9efdcaa3c168ec5e68dcc18d81
ãJaehun3515 <jiro14@nate.com>
2025-05-08 16:17:28 +0900

    commit

--- pi_study/switch/switch3.py
+++ pi_study/switch/switch3.py
@@ -3,7 +3,27 @@ from time import sleep
 
 gpio.setmode(gpio.BCM)
 
+
 class Led:
+
+    def __init__(self, pin, color):
+        self.pin = pin
+        self.color = color
+        gpio.setup(pin, gpio.OUT)
+        gpio.output(pin, gpio.LOW)
+
+    def blink(self, count, time):
+        for _ in range(count):
+            gpio.output(self.pin, gpio.HIGH)
+            sleep(time)
+            gpio.output(self.pin, gpio.LOW)
+            sleep(time)
+
+    def ledOn(self):
+        gpio.output(self.pin, gpio.HIGH)
+
+    def ledOff(self):
+        gpio.output(self.pin, gpio.LOW)
 
 
 class Button:
